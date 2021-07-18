4efc40043e276117a6d34dcfc9ee7ed898fbc5b2
Kevin Kühnemund <32802935+jabali2004@users.noreply.github.com>
2021-07-18 23:47:35 +0200

    Add BeekeeperBackend project

---
+++ BeekeeperBackend/BeekeeperContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using BeekeeperBackend.Models;
+
+#nullable disable
+
+namespace BeekeeperBackend
+{
+    public partial class BeekeeperContext : DbContext
+    {
+        public BeekeeperContext()
+        {
+        }
+
+        public BeekeeperContext(DbContextOptions<BeekeeperContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
